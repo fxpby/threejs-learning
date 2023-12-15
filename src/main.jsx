09970ddfa995e7f18011f51379e04bd30a9e8e34
fxpby <fxpby@outlook.com>
2023-12-15 15:05:05 +0800

    chore: add less, unocss and readt-icons

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import MainLayout from './Layouts/MainLayout'
 import ErrorPage from './pages/ErrorPage'
 import Exercise from './pages/Exercises'
+import 'virtual:uno.css'
 import './index.css'
 
 const router = createBrowserRouter([
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [UnoCSS(), react()],
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: 'always',
+      },
+    },
+  },
 })
