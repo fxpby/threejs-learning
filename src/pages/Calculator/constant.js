718f49cfe42e2286e2ecb5668c7ad62ac8cf04c1
fxpby <fxpby@outlook.com>
2024-07-25 19:09:52 +0800

    feat: add constant

--- src/pages/Calculator/constant.js
+++ src/pages/Calculator/constant.js
@@ -1,45 +1,77 @@
-export const buffer = 0.15
+export const buffer = {
+  'mxs-1': 0.15,
+}
 
-export const lightTraining = 0.7
+export const lightTrainingDegree = {
+  'mxs-1': 0.7,
+  'mxs-2': 0.85,
+}
 
-export const overloadIncreaseDegree = 0.02
+export const overloadIncreaseDegree = {
+  'mxs-1': 0.02,
+  'mxs-2': 0.05,
+}
 
 export const cycleConfig = {
   'mxs-1': {
-    group: [[4, 5, 5, 3, 5], [4, 4, 4, 3, 5]],
-    count: [[6, 5, 5, 5, 5], [5, 4, 3, 3, 3]]
+    group: [
+      [4, 5, 5, 3, 5],
+      [4, 4, 4, 3, 5],
+    ],
+    count: [
+      [6, 5, 5, 5, 5],
+      [5, 4, 3, 3, 3],
+    ],
   },
   'mxs-2': {
     group: [[], []],
-    count: [[], []]
-  }
+    count: [[], []],
+  },
 }
 
 export const tableColumn = [
   {
-    label: '/',
-    id: 'name',
-    items: rawList,
+    label: '周',
+    id: 'week',
   },
   {
-    label: '适应次数',
-    id: 'rep',
-    items: [],
-  },
-  {
-    label: '目标负荷',
-    id: 'targetLoad',
-    items: [],
-  },
-  {
-    label: '次',
-    id: 'count',
-    items: [],
-  },
-  {
-    label: '组',
+    label: '组数',
     id: 'group',
-    items: [3, 4, 5, 4, 6],
   },
-];
+  {
+    label: '次数',
+    id: 'count',
+  },
+  {
+    label: '绝对强度',
+    id: 'absoluteStrength',
+  },
+  {
+    label: '相对强度',
+    id: 'relativeStrength',
+  },
+  {
+    label: '训练负荷',
+    id: 'trainingLoad',
+  },
+  {
+    label: '轻训',
+    id: 'lightTraining',
+  },
+]
 
+export const RMCountRelation = {
+  1: 1,
+  2: 0.95,
+  3: 0.93,
+  4: 0.9,
+  5: 0.87,
+  6: 0.85,
+  7: 0.83,
+  8: 0.8,
+  9: 0.77,
+  10: 0.75,
+  11: 0.7,
+  12: 0.67,
+  15: 0.65,
+}
